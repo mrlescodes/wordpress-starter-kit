f4734c1d4c42d23671751231c39f0a566c5ad52b
Les Aubrey <hello@mrles.co.uk>
2020-12-17 13:05:36 +0700

    feat: adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true, // So parent files don't get applied
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:import/recommended',
+    'airbnb-base',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'prettier',
+  ],
+  rules: {
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    'import/prefer-default-export': 'off',
+
+    // Missing yarn workspace support
+    'import/no-extraneous-dependencies': 'off',
+
+    // Allow disabling of eslint for a whole file
+    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+
+    // Enable uppercase Component filenames
+    'unicorn/filename-case': 'off',
+
+    // Common abbreviations are known and readable
+    'unicorn/prevent-abbreviations': 'off',
+  },
+};
