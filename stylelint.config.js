ed16ca700b1f374b298a806e2d9ed24c813ab2b6
Les Aubrey <hello@mrles.co.uk>
2020-12-18 14:13:58 +0700

    feat: pre-commit tooling adjustments

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,10 @@
+/* eslint-disable unicorn/no-null */
+
 module.exports = {
   extends: 'stylelint-config-wordpress',
+  rules: {
+    'declaration-property-unit-whitelist': null,
+    'max-line-length': null,
+    'no-empty-source': null,
+  },
 };
