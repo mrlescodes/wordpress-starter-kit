466e9a795740d0ec8e17bb43553a3f6e5ebf4abf
Les Aubrey <hello@mrles.co.uk>
2020-12-20 13:12:07 +0700

    fix: nothing found template layout

--- wp-content/themes/wsk-theme/template-parts/layouts/nothing-found.php
+++ wp-content/themes/wsk-theme/template-parts/layouts/nothing-found.php
@@ -11,18 +11,16 @@
 
 <section class="layout layout--nothing-found">
 	<div class="container-fluid">
-		<div class="row">
 
-			<?php
-			if ( is_search() ) :
-				printf( '<p class="lead">%s</p>', esc_html__( 'Sorry, but nothing matched your search term.', 'wsk-theme' ) );
-			else :
-				printf( '<p class="lead">%s</p>', esc_html__( 'It looks like nothing was found at this location.', 'wsk-theme' ) );
-			endif;
+		<?php
+		if ( is_search() ) :
+			printf( '<p class="lead">%s</p>', esc_html__( 'Sorry, but nothing matched your search term.', 'wsk-theme' ) );
+		else :
+			printf( '<p class="lead">%s</p>', esc_html__( 'It looks like nothing was found at this location.', 'wsk-theme' ) );
+		endif;
 
-			printf( '<a href="%s" class="btn btn-primary">%s</a>', esc_url( get_site_url() ), esc_html__( 'Return home', 'wsk-theme' ) );
-			?>
+		printf( '<a href="%s" class="btn btn-primary">%s</a>', esc_url( get_site_url() ), esc_html__( 'Return home', 'wsk-theme' ) );
+		?>
 
-		</div>
 	</div>
 </section>
