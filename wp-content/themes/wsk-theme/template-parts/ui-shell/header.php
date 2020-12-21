8ef308b8b789d3973e7613a3ddc7e6967b9ccae7
Les Aubrey <hello@mrles.co.uk>
2020-12-21 11:10:02 +0700

    feat: Adds contact link to header

--- wp-content/themes/wsk-theme/template-parts/ui-shell/header.php
+++ wp-content/themes/wsk-theme/template-parts/ui-shell/header.php
@@ -5,6 +5,9 @@
  * @package WSK_Theme
  */
 
+$fields = array(
+	'contact_page_url' => get_field( 'contact_page', 'option' ),
+);
 ?>
 
 <header id="masthead" class="site-header" role="banner">
@@ -15,11 +18,17 @@
 				<?php bloginfo( 'name' ); ?>
 			</a>
 
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo esc_attr__( 'Toggle navigation', 'wsk-theme' ); ?>">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'wsk-theme' ); ?>">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 
 			<?php wskt_main_menu(); ?>
+
+			<?php
+			if ( $fields['contact_page_url'] ) :
+				printf( '<a href="%s" class="btn btn-primary">%s</a>', esc_url( $fields['contact_page_url'] ), esc_attr__( 'Contact', 'wsk-theme' ) );
+			endif;
+			?>
 
 		</div>
 	</nav>
