16045469ada0a2a80f873d0ade3b03887cca7b0c
Les Aubrey <hello@mrles.co.uk>
2020-12-22 17:50:16 +0700

    feat: adds page template

---
+++ wp-content/themes/wsk-theme/page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template for pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WSK_Theme
+ */
+
+get_header();
+?>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : ?>
+
+			<?php the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content/page' ); ?>
+
+		<?php endwhile; ?>
+
+	</main>
+
+<?php
+get_footer();
---
+++ wp-content/themes/wsk-theme/template-parts/content/page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for page content
+ *
+ * @package WSK_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php wskt_content_builder(); ?>
+
+</article>
