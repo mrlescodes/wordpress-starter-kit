e64392c5ce8397a931097a8bf31c1cddde64522c
Les Aubrey <hello@mrles.co.uk>
2021-01-20 14:09:19 +0700

    Fix: Fix call to action background image

--- wp-content/themes/wsk-theme/template-parts/layouts/call-to-action.php
+++ wp-content/themes/wsk-theme/template-parts/layouts/call-to-action.php
@@ -10,22 +10,29 @@ $fields = array(
 	'title'            => get_sub_field( 'title' ),
 	'contact_page_url' => get_field( 'contact_page', 'option' ),
 );
+
+$layout_classes_args = array(
+	'layout_name'   => 'call-to-action',
+	'colour_scheme' => 'dark',
+);
 ?>
 
-<section class="<?php wskt_layout_classes( 'call-to-action' ); ?>" <?php wskt_bg_image_styles( $fields['background_image'] ); ?>>
+<section class="<?php wskt_layout_classes( $layout_classes_args ); ?>" <?php wskt_bg_image_styles( $fields['background_image']['image'] ); ?>>
 	<div class="container-fluid">
 
-		<?php
-		if ( $fields['title'] ) {
-			printf( '<h2>%s</h2>', esc_attr( $fields['title'] ) );
-		}
-		?>
+		<div class="layout__content">
+			<?php
+			if ( $fields['title'] ) {
+				printf( '<h2>%s</h2>', esc_attr( $fields['title'] ) );
+			}
+			?>
 
-		<?php
-		if ( $fields['contact_page_url'] ) {
-			printf( '<a href="%s">%s</a>', esc_url( $fields['contact_page_url'] ), esc_attr__( "Let's talk", 'wsk-theme' ) );
-		}
-		?>
+			<?php
+			if ( $fields['contact_page_url'] ) {
+				printf( '<h1><a href="%s">%s</a></h1>', esc_url( $fields['contact_page_url'] ), esc_attr__( "Let's talk", 'wsk-theme' ) );
+			}
+			?>
+		</div>
 
 	</div>
 </section>
