1a1c630c019a089879850b804dd404ec5bddbfd2
Les Aubrey <hello@mrles.co.uk>
2021-01-20 17:47:32 +0700

    Add image layout and styling

--- wp-content/themes/wsk-theme/template-parts/layouts/image.php
+++ wp-content/themes/wsk-theme/template-parts/layouts/image.php
@@ -6,23 +6,31 @@
  */
 
 $fields = array(
-	'image' => get_sub_field( 'image' ),
+	'image'         => get_sub_field( 'image' ),
+	'colour_scheme' => get_sub_field( 'colour_scheme' ),
 );
 
 if ( empty( $fields['image'] ) ) {
 	return;
 }
+
+$layout_classes_args = array(
+	'layout_name'   => 'image',
+	'colour_scheme' => $fields['colour_scheme'],
+);
 ?>
 
-<section class="<?php wskt_layout_classes( 'image' ); ?>">
+<section class="<?php wskt_layout_classes( $layout_classes_args ); ?>">
+
 	<div class="container-fluid">
 		<div class="row">
 
 			<div class="col-lg-10 offset-lg-1">
 				<!-- TODO: Set correct image size -->
-				<?php echo wp_get_attachment_image( $fields['image']['id'], 'full' ); ?>
+				<?php echo wp_get_attachment_image( $fields['image'], 'full', '', array( 'class' => 'layout__image' ) ); ?>
 			</div>
 
 		</div>
 	</div>
+
 </section>
