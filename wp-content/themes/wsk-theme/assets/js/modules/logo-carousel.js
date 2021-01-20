e907f70ece67344353563de54e285c69f472c930
Les Aubrey <hello@mrles.co.uk>
2021-01-20 11:37:44 +0700

    Fix: logo carousel bug when markup not present

--- wp-content/themes/wsk-theme/assets/js/modules/logo-carousel.js
+++ wp-content/themes/wsk-theme/assets/js/modules/logo-carousel.js
@@ -1,19 +1,23 @@
 import { tns } from 'tiny-slider/src/tiny-slider';
 
-tns({
-  container: '.logo-carousel',
-  controls: false,
-  nav: false,
-  items: 2,
-  responsive: {
-    640: {
-      items: 4,
+if ($('.logo-carousel').length > 0) {
+  tns({
+    container: '.logo-carousel',
+    controls: true,
+    nav: false,
+    items: 2,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    responsive: {
+      640: {
+        items: 3,
+      },
+      800: {
+        items: 4,
+      },
+      960: {
+        items: 5,
+      },
     },
-    700: {
-      items: 6,
-    },
-    900: {
-      items: 8,
-    },
-  },
-});
+  });
+}
